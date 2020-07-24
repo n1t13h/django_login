e7fa13e1babfa708194ff2119876f6aab1e89e76
nitish <nitishp74@gmail.com>
2020-07-24 12:51:46 +0530

    added login logout register views

--- users/urls.py
+++ users/urls.py
@@ -2,4 +2,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('',views.homepage,name="home"),
+    path('register',views.register,name="register"),
+    path('logout',views.logout_request,name="logout"),
+    path('login',views.login_request,name="login"),
 ]
